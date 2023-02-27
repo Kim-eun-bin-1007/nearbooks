1ddc522189036a30c9f0ae0d2b779c0139bc2f31
eunbin <dmsqls1007@naver.com>
2023-02-27 23:47:51 +0900

    🎨 [Home] 스타일 변경 및 텍스트 추가

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,58 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import { HomeWrap, InfoType } from '../style/Home';
+import { HomeWrap } from "../style/Home";
 
 function Home() {
+  const category = [
+    {
+      link: "borough",
+      title: (
+        <>
+          자치구별
+          <br /> 도서관 정보
+        </>
+      ),
+      img: "/assets/icon/library.png",
+    },
+    {
+      link: "map",
+      title: (
+        <>
+          내 주변
+          <br /> 도서관 정보
+        </>
+      ),
+      img: "/assets/icon/map.png",
+    },
+  ];
+
   return (
     <HomeWrap>
-      <InfoType>
-        <li className="info-type__item">
-          <Link to="borough">
-            <h2 className="info-type__title">자치구별 도서관 정보</h2>
-          </Link>
-        </li>
-        <li className="info-type__item">
-          <Link to="map">
-            <h2 className="info-type__title">내 주변 도서관 정보</h2>
-          </Link>
-        </li>
-      </InfoType>
+      <p className="desc">
+        NearbyBooks는 서울 시내의 공공도서관과 작은도서관의 정보를 제공하는
+        서비스입니다.
+      </p>
+
+      <ul className="category">
+        {category.map((item, i) => {
+          return (
+            <li className="category__item" key={i}>
+              <Link to={item.link} className="category__link">
+                <h2 className="category__title">{item.title}</h2>
+                <img src={item.img} alt="" className='category__img' />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+
+      <p className="tip">
+        위치정보 제공에 동의하시면 현재위치를 기반으로 가까운 도서관을 찾을 수
+        있습니다.
+        <br />
+        위치정보 설정 후 <span className="tip__em">'내 주변 도서관 정보'</span>를
+        이용해주세요 :)
+      </p>
     </HomeWrap>
   );
 }
--- src/style/Home.js
+++ src/style/Home.js
@@ -1,24 +1,78 @@
 import styled from 'styled-components';
 
 export const HomeWrap = styled.div`
-  min-height: 100vh;
-  // background-color: #fdf1ed;
-`;
+  padding: 100px 15px 60px;
 
-export const InfoType = styled.ul`
-  padding: 0 15px;
-  padding-top: 60px;
+  .desc {
+    margin: 0 auto;
+    padding: 20px;
+    padding-bottom: 40px;
+    max-width: 360px;
+    font-size: 16px;
+    color: #444;
+    line-height: 1.5;
+    text-align: center;
+    word-break: keep-all;
+    word-wrap: break-word;
+  }
 
-  .info-type {
+  .category {
+    display: flex;
+    justify-content: space-between;
+
     &__item {
-      margin-bottom: 40px;
-      background-color: #fdf1ed;
-      border-radius: 5px;
+      margin-bottom: 30px;
+      width: calc(50% - 10px);
     }
-    
+
+    &__link {
+      box-sizing: border-box;
+      display: block;
+      position: relative;
+      padding: 15px 15px 80px;
+      background-color: #fdf1ed;
+      box-shadow: 10px 10px 15px rgba(0, 0, 0, 0.04);
+      border-radius: 5px;
+      transition: 0.3s;
+
+      &:hover,
+      &:focus {
+        background-color: #f8d4c9;
+        box-shadow: 10px 10px 15px rgba(0, 0, 0, 0.2);
+      }
+    }
+
     &__title {
-      padding: 30px 0;
-      text-align: center;
+      margin: 0;
+      font-size: 14px;
+    }
+
+    &__img {
+      display: block;
+      position: absolute;
+      bottom: 15px;
+      right: 15px;
+      margin-top: 20px;
+      width: 28px;
+      height: 28px;
+    }
+  }
+
+  .tip {
+    margin: 0 auto;
+    padding: 20px;
+    max-width: 500px;
+    border: 1px solid #eee;
+    border-radius: 5px;
+    font-size: 12px;
+    color: #666;
+    line-height: 1.5;
+    text-align: center;
+    word-break: keep-all;
+    word-wrap: break-word;
+
+    .tip__em {
+      color: #111;
     }
   }
 `;
