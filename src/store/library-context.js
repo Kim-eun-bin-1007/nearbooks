1b9a85af53fd4e363db212b267df9f543ac3571e
eunbin <dmsqls1007@naver.com>
2023-03-16 22:09:09 +0900

    ♻️ nearbybooks api 사용하여 데이터 fetch
    - 불필요한 코드 제거

--- src/store/library-context.js
+++ src/store/library-context.js
@@ -10,99 +10,26 @@ export const LibraryCtx = React.createContext({
   getLibraryList: () => {},
 });
 
-let isFetchLibrary = false;
-const api =
-  "http://openapi.seoul.go.kr:8088/6c6b476c48646d73373141446f5962/json/";
-const publicString = "SeoulPublicLibraryInfo";
-const smallString = "SeoulSmallLibraryInfo";
-
 const LibraryProvider = (props) => {
   const [publicTotal, setPublicTotal] = useState(0);
   const [smallTotal, setSmallTotal] = useState(0);
   const [publicLibrary, setPublicLibrary] = useState(null);
   const [smallLibrary, setSmallLibrary] = useState(null);
 
-  // http 요청
-  const getRequest = (api) => {
-    return fetch(api)
-      .then((response) => response.json())
-      .catch((err) => err);
-  };
-
-  // 구코드로 데이터 분류
-  const classifyData = (data, updateStateFn) => {
-    let separateData = {};
-
-    data.forEach((item) => {
-      if (separateData[item.GU_CODE]) {
-        separateData[item.GU_CODE].push(item);
-      } else {
-        const newGuCode = { [item.GU_CODE]: [item] };
-        separateData = { ...separateData, ...newGuCode };
-      }
-    });
-
-    updateStateFn(separateData);
-  };
-
-  // 도서관 정보 가져오기
-  const getLibraryList = useCallback((api, libraryString, updateStateFn) => {
-    if (isFetchLibrary) return;
-
-    getRequest(`${api}${libraryString}/0/999/`).then((res) => {
-      const total = res[libraryString]?.list_total_count;
-
-      if (libraryString === "SeoulPublicLibraryInfo") {
-        setPublicTotal(total);
-      } else {
-        setSmallTotal(total);
-      }
-
-      if (total >= 1000) {
-        // 최대 1000건만 조회가능하여 초과할경우 분할하여 데이터 수신
-        const turn = Math.ceil(total / 1000);
-        const remainNum = total % 1000;
-        let newLibrary = [...res[libraryString].row];
-
-        for (let i = 2; i <= turn; i++) {
-          const START_INDEX = 1000 * i - 1000;
-          const END_INDEX = 1000 * i - 1;
-
-          getRequest(
-            `${api}${libraryString}/${START_INDEX}/${END_INDEX}/`
-          ).then((response) => {
-            // 마지막 반복이 아닐경우 동작
-            if (i !== turn) {
-              newLibrary.push(...response[libraryString].row);
-              return;
-            }
-
-            // 마지막 반복에서만 동작
-            if (remainNum === 0) {
-              classifyData(newLibrary, updateStateFn);
-            } else {
-              const result = [...newLibrary, ...response[libraryString].row];
-              classifyData(result, updateStateFn);
-            }
-          });
-        }
-      } else {
-        classifyData(res[libraryString].row, updateStateFn);
-      }
-
-      isFetchLibrary = true;
-    });
-  }, []);
-
   const getLibraryData = useCallback(() => {
-    // 공공도서관
-    isFetchLibrary = false;
-    getLibraryList(api, publicString, setPublicLibrary);
+    const api =
+      "https://port-0-nearbybooks-api-6g2llfar2z9i.sel3.cloudtype.app/";
 
-    // 작은도서관
-    isFetchLibrary = false;
-    getLibraryList(api, smallString, setSmallLibrary);
-  }, [getLibraryList]);
+    fetch(api)
+      .then((response) => response.json())
+      .then((res) => { 
+        setPublicTotal(res.total.public);
+        setSmallTotal(res.total.small);
+        setPublicLibrary(res.publicLibrary);
+        setSmallLibrary(res.smallLibrary);
+      })
+      .catch((err) => err);
+  }, []);
 
   const libraryCtx = {
     total: {
